dfc3f310850c3ec14a0e5974302c58be79ee2f78
Evgeni <666997@bk.ru>
2022-04-23 21:07:58 +0800

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum(int number)
+{   
+    int resault = 0;
+    while (number > 0) 
+    {
+        resault = resault + number % 10;
+        number /= 10;
+    }
+    return resault;
+}
+System.Console.WriteLine("452 -> " + sum(452));
+System.Console.WriteLine("82 -> " + sum(82));
+System.Console.WriteLine("9012 -> " + sum(9012));
