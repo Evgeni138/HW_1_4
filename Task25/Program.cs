2b3bd7085ca21d0d42775932bf92a92810fd1795
Evgeni <666997@bk.ru>
2022-04-23 20:46:45 +0800

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿int degree(int a, int b)
+{
+    int resault = a;
+    for (int i = 1; i < b; i++)
+    {
+        resault = resault * a;
+    }
+    return resault;
+}
+
+System.Console.WriteLine("3, 5 -> " + degree(3, 5));
+System.Console.WriteLine("2, 4 -> " + degree(2, 4));
