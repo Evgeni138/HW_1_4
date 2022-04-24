bd0903f23f4b51cd030e8f37c4a10b55e49d8000
Evgeni <666997@bk.ru>
2022-04-24 08:45:09 +0800

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите по очереди {a} элементов массива");
+void createArray(int[] array)
+{
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void printArray(int[] array1)
+{
+    for (int j = 0; j < a; j++)
+    {
+        Console.Write($"{array1[j]} ");
+    }
+}
+int[] arr = new int[a];
+createArray(arr);
+Console.WriteLine("Массив:");
+printArray(arr);
